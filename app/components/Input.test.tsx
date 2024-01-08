bfd683844b2795fb1da7b2995954f0e395f4cb9d
Collin Gowdy <nolic0321@gmail.com>
2024-01-08 07:40:18 -0600

    Now it passes

--- app/components/Input.test.tsx
+++ app/components/Input.test.tsx
@@ -3,7 +3,7 @@ import {render, screen} from "@testing-library/react";
 
 it('should render the component', () => {
     let inputText = "test";
-   render(<Input onInputChanged={()=>{}} inputText={inputText}/>)
-   expect(screen.getByText(inputText)).toBeInTheDocument();
+   render(<Input onInputChanged={(value:string)=>{ inputText = value;}} inputText={inputText}/>)
+   expect(screen.getByDisplayValue(inputText)).toBeInTheDocument();
 });
 
