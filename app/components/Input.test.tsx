2de03bc2d1269e000a8947599ddfe00b4dbbcbd1
Collin Gowdy <nolic0321@gmail.com>
2024-01-08 07:27:48 -0600

    This should fail

---
+++ app/components/Input.test.tsx
@@ -0,0 +1,8 @@
+import Input from "@components/Input";
+import {render, screen} from "@testing-library/react";
+
+it('should render the component', () => {
+    let inputText = "test";
+   render(<Input onInputChanged={()=>{}} inputText={inputText}/>)
+   expect(screen.getByText(inputText)).toBeInTheDocument();
+});
