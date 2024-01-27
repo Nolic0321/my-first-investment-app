085f742829b8d665c2cab3be0b19dc8dd5d83d14
Collin Gowdy <nolic0321@gmail.com>
2024-01-26 19:25:37 -0600

    Split up some bigger specs and updated the YAML to only do chromium on master

---
+++ tests/e2e/parent/dashboard/requests/mongoRequestApprove.spec.ts
@@ -0,0 +1,29 @@
+import {expect, test} from '@playwright/test';
+import {createMongoChildRequest, loginAsMongoParent} from "@playwrightHelpers";
+
+test.describe('Mongo Request Approval', () => {
+    test.beforeEach(async ({page}) => {
+        await createMongoChildRequest(page);
+        await page.getByRole('link', {name: 'Logout'}).click();
+        await loginAsMongoParent(page);
+    });
+
+    test('should approve request', async ({page}) => {
+        let requestCalled = false;
+        const requestToTest = page.getByText('Request $10').first();
+        await expect(requestToTest).toBeVisible();
+        const elementId = await requestToTest.getAttribute('id')??'';
+        await page.route(`**/api/transactions/*/approve`, async (route) => {
+            console.log('route called');
+            requestCalled = true;
+            await route.fulfill({
+                status: 200
+            });
+        });
+        await page.getByRole('button', {name: 'Approve'}).first().click();
+        const thing = page.getByTestId(elementId);
+        await expect(thing).toBeHidden();
+        expect(requestCalled).toBeTruthy();
+    });
+
+});
---
+++ tests/e2e/parent/dashboard/requests/mongoRequestDeny.spec.ts
@@ -0,0 +1,27 @@
+import {expect, test} from '@playwright/test';
+import {createMongoChildRequest, loginAsMongoParent} from "@playwrightHelpers";
+
+test.describe('Mongo Request Denial', () => {
+    test.beforeEach(async ({page}) => {
+        await createMongoChildRequest(page);
+        await page.getByRole('link', {name: 'Logout'}).click();
+        await loginAsMongoParent(page);
+    });
+
+    test('should deny request', async ({page}) => {
+        let requestCalled = false;
+        await expect(page.getByText('Request $10')).toBeVisible();
+        await page.route(`**/api/transactions/*/deny`, async (route) => {
+            console.log('route called');
+            requestCalled = true;
+            await route.fulfill({
+                status: 200
+            });
+        });
+        await page.getByRole('button', {name: 'Deny'}).first().click();
+        const thing = page.getByText('Request $10');
+        await expect(thing).toBeHidden();
+        expect(requestCalled).toBeTruthy();
+    });
+
+});
