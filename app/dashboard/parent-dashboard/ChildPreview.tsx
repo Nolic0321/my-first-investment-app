b36be61a8c74a9861cbf272735721b034e732167
Collin Gowdy <nolic0321@gmail.com>
2024-02-05 11:02:34 -0600

    Wrap up unit tests around Contexts

---
+++ app/dashboard/parent-dashboard/ChildPreview.tsx
@@ -0,0 +1,10 @@
+import {ChildAccount} from "@models/child-account";
+
+export type ChildPreviewProps = {
+    child: ChildAccount;
+    onChildSelected: (child: ChildAccount) => void;
+}
+
+export function ChildPreview(props:ChildPreviewProps){
+
+}
